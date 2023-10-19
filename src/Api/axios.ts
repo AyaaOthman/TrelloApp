45199a6ac227c4d78a8f8e43cb984493fe50911d
Aya Othman <ayaaothman96@gmail.com>
2023-10-19 15:02:18 +0300

    fix axios ts

---
+++ src/Api/axios.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const baseURL = "https://trello-app-iti.onrender.com";
+
+export const axiosInstance = axios.create({
+  baseURL,
+});
--- src/Views/Auth/Register/Register.tsx
+++ src/Views/Auth/Register/Register.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import * as yup from "yup";
 import { useFormik } from "formik";
-import { IRegister } from "../../../interfaces/register.interface";
+import { IRegister } from "../../../Interfaces/register.interface";
 import { axiosInstance } from "../../../Api/axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
