33f997fbd9bf6f6e6a9b38bffd770a4a4bd9d01d
Aya Othman <ayaaothman96@gmail.com>
2023-10-19 14:56:45 +0300

    merge master to branch

--- src/Api/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const baseURL = "https://trello-app-iti.onrender.com"
-
-export const axiosInstance = axios.create({
-    baseURL
-})
--- src/Views/Tasks/index.tsx
+++ src/Views/Tasks/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import TaskCard from "../../Components/UI/Cards/TaskCard";
+import AddTask from "../../Components/NewTask";
 
 const TasksPage = () => {
   return (
     <main>
+      <div className="container">
+        <AddTask />
+      </div>
       <div className="container">
         <div className="grid grid-cols-3 gap-4">
           <TaskCard
