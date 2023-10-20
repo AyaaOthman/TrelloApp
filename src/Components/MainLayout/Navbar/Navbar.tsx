027ddbad1909a789145d1f41e4cb202009f31020
Aya Othman <ayaaothman96@gmail.com>
2023-10-20 12:01:37 +0300

    added  new task modal

--- src/Components/MainLayout/Navbar/Navbar.tsx
+++ src/Components/MainLayout/Navbar/Navbar.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 // import styles from "./Navbar.module.css";
 export default function Navbar() {
-  return (
-    <nav className=" flex justify-content-around py-1 px-6 bg-bluee shadow  w-full">
-      <div className="max-w-7xl flex flex-col text-center align-items-center sm:flex-row sm:text-left sm:justify-between sm:items-baseline  w-full">
-        <div className="flex align-items-center ">
-          <img src="./logo.png" alt="logo" className="w-16" />
-          <h4 className="text-Orange">Todo App</h4>
-        </div>
-        <div>
-          <Link
-            to="/register"
-            className="text-lg fw-bold text-white py-2 px-3 no-underline hover:bg-Orange rounded-2 ml-2"
-          >
-            Register
-          </Link>
-          <Link
-            to="/login"
-            className="text-lg fw-bold text-white no-underline py-2 px-3 hover:bg-Orange rounded-2  ml-2"
-          >
-            Login
-          </Link>
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className=" flex justify-center py-1 px-6 bg-black shadow  w-full">
+            <div className="container flex flex-col text-center items-center sm:flex-row sm:text-left sm:justify-between sm:items-baseline  w-full">
+                <div className="flex items-center ">
+                    <img src="./logo.png" alt="logo" className="w-16" />
+                    <h4 className="text-Orange text-xl font-bold">Todo App</h4>
+                </div>
+                <div>
+                    <Link
+                        to="/register"
+                        className="text-lg font-bold text-white py-2 px-3 no-underline hover:bg-Orange rounded-md ml-2"
+                    >
+                        Register
+                    </Link>
+                    <Link
+                        to="/login"
+                        className="text-lg font-bold text-white no-underline py-2 px-3 hover:bg-Orange rounded-md  ml-2"
+                    >
+                        Login
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
 }
--- src/Views/Tasks/index.tsx
+++ src/Views/Tasks/index.tsx
@@ -1,49 +1,52 @@
-import React from "react";
-import TaskCard from "../../Components/UI/Cards/TaskCard";
-import AddTask from "../../Components/NewTask";
+import React from 'react'
+import TaskCard from '../../Components/UI/Cards/TaskCard'
+import AddTask from '../../Components/NewTask'
 
 const TasksPage = () => {
-  return (
-    <main>
-      <div className="container">
-        <AddTask />
-      </div>
-      <div className="container">
-        <div className="grid grid-cols-3 gap-4">
-          <TaskCard
-            className=" rounded-lg border p-3"
-            title="Hello"
-            description="lorem sdf;ldvs"
-            status="doing"
-          />
-          <TaskCard
-            className=" rounded-lg border p-3"
-            title="Hello"
-            description="lorem sdf;ldvs"
-            status="doing"
-          />
-          <TaskCard
-            className=" rounded-lg border p-3"
-            title="Hello"
-            description="lorem sdf;ldvs"
-            status="doing"
-          />
-          <TaskCard
-            className=" rounded-lg border p-3"
-            title="Hello"
-            description="lorem sdf;ldvs"
-            status="doing"
-          />
-          <TaskCard
-            className=" rounded-lg border p-3"
-            title="Hello"
-            description="lorem sdf;ldvs"
-            status="doing"
-          />
-        </div>
-      </div>
-    </main>
-  );
-};
+    return (
+        <main className="container flex flex-col justify-center mx-auto">
+            <div className="container text-center my-6">
+                <h2 className="text-2xl font-bold mb-5 ">
+                    Failing to plan is simply planning to fail !
+                </h2>
+                <AddTask />
+            </div>
+            <div className="container">
+                <div className="grid grid-cols-3 gap-4">
+                    <TaskCard
+                        className=" rounded-lg border p-3"
+                        title="Hello"
+                        description="lorem sdf;ldvs"
+                        status="doing"
+                    />
+                    <TaskCard
+                        className=" rounded-lg border p-3"
+                        title="Hello"
+                        description="lorem sdf;ldvs"
+                        status="doing"
+                    />
+                    <TaskCard
+                        className=" rounded-lg border p-3"
+                        title="Hello"
+                        description="lorem sdf;ldvs"
+                        status="doing"
+                    />
+                    <TaskCard
+                        className=" rounded-lg border p-3"
+                        title="Hello"
+                        description="lorem sdf;ldvs"
+                        status="doing"
+                    />
+                    <TaskCard
+                        className=" rounded-lg border p-3"
+                        title="Hello"
+                        description="lorem sdf;ldvs"
+                        status="doing"
+                    />
+                </div>
+            </div>
+        </main>
+    )
+}
 
-export default TasksPage;
+export default TasksPage
